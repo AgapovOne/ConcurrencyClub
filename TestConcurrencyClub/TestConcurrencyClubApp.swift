e0ce52a24f22d771e008d2b7bbd8c3fb954f51f8
AgapovOne <agapov.one@gmail.com>
2024-06-07 08:43:52 +0500

    Initial Commit

---
+++ TestConcurrencyClub/TestConcurrencyClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestConcurrencyClubApp.swift
+//  TestConcurrencyClub
+//
+//  Created by Alex Agapov on 07.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestConcurrencyClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
